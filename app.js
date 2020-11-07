d9b169ef8d9e9c97242ab3a36dc01f2bfaeb8062
Dawit Ayana <dawitz28@gmail.com>
2020-11-06 20:25:29 -0800

    prob-5

--- app.js
+++ app.js
@@ -36,11 +36,11 @@ function multiply(a, b) { //eslint-disable-line
     console.log([product, str]);
 
     return [product, str];
-    
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -57,7 +57,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    var firstSum = sum(a,b)[0];
+    var firstSum = sum(a, b)[0];
     var collectiveSum = sum(c, firstSum)[0];
     var product = multiply(a, multiply(b, c)[0])[0];
 
@@ -68,7 +68,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -86,8 +86,18 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+var testArray = [2, 3, 4]; //eslint-disable-line
+
+function sumArray(sumArr) { //eslint-disable-line
+    var returnArray = [];
+    var aPlusb = sum((sumArr[0]), (sum[1]));
+    aPlusb = aPlusb[0];
+    var finalSum = sum(aPlusb, sumArr[2])[0];
+    returnArray[0] = finalSum;
+    returnArray[1] = `${sumArr[0]}, ${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${finalSum} is their sum.`
 
 }
+return returnArray;
 
 // Here is the test for sumArray(); uncomment it to run it
 
@@ -106,12 +116,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
 
-}
+function multiplyArray(multArr) { //eslint-disable-line
+    var Array = [];
+    // calculate product of first two values
+    var aTimesb = multiply((multArr[0]), (multArr[1]));
+    aTimesb = aTimesb[0];
+    var finalProduct = multiply(aTimesb, multArr[2]);
+    finalProduct = finalProduct[0];
+    Array[0] = finalProduct;
+    Array[1] = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${finalProduct}.`;
+    return Array;
+  }
+  testMultiplyArray(testArray);
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -132,13 +151,12 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+// var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+// function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
-}
+// }
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas. 
